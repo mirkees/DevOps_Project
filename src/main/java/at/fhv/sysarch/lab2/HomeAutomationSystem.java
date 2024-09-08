389c042f13b4b303f03a868aeddd99fc7590f2ff
root <m.elmenreich@gmail.com>
2024-09-08 15:08:25 +0000

    test with docker container 2

--- src/main/java/at/fhv/sysarch/lab2/HomeAutomationSystem.java
+++ src/main/java/at/fhv/sysarch/lab2/HomeAutomationSystem.java
@@ -9,6 +9,6 @@ public class HomeAutomationSystem {
         ActorSystem<Void> home = ActorSystem.create(HomeAutomationController.create(), "HomeAutomation");
     }
 
-    //Das ist test mit docker containe 1  
+    //Das ist test mit docker container  
 
 }
