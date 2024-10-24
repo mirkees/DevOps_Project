10c8790b8a3de7c1065196636d27e2467fa258f4
Markus Elmenreich <markuselmenreich@MBP-von-Markus.fritz.box>
2024-10-24 17:08:16 +0200

    final test

---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,11 @@
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleTest {
+    @Test
+    public void testHelloWorld() {
+        String hello = "Hello World";
+        assertEquals("Hello World", hello);
+    }
+}
